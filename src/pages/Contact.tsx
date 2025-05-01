c4fe83f0e635b0433b65735252bd46dc6dbfd3d9
tristan-gscn <tristan.gscn@gmail.com>
2025-05-01 17:47:19 +0200

    Refactor ContactField component to accept placeholder prop and update input placeholder usage

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,15 +1,17 @@
 import NavBar from "@/components/home/nav/NavBar";
 import SemiBold from "@/components/style/SemiBold";
+import React from "react";
 
 interface ContactFieldProps {
     name: string;
     type: string;
+    placeholder?: string;
 }
-const ContactField: React.FC<ContactFieldProps> = ({ name, type }) => {
+const ContactField: React.FC<ContactFieldProps> = ({ name, type, placeholder }) => {
     return (
         <div>
             <p className="text-center text-xl font-light">{name}</p>
-            <input className="relative left-1/2 -translate-x-1/2 text-center text-light w-80 py-1 mt-2 mb-5.5 rounded-lg border border-gray-300 dark:border-gray-700" type={type} placeholder={name} />
+            <input className="relative left-1/2 -translate-x-1/2 text-light w-80 pl-2 py-1 mt-2 mb-5.5 rounded-lg border border-gray-300 dark:border-gray-700" type={type} placeholder={placeholder} />
         </div>
     )
 }
@@ -19,16 +21,21 @@ const Contact = () => {
         <div className="h-screen relative">
             <NavBar />
             <h1 className="my-20 text-center text-7xl font-black">
-                Contact <span className="text-blue-500">us</span>
+                Contact <span className="text-blue-500">Us</span>
             </h1>
 
             <ContactField name="Name" type="text" />
-            <ContactField name="Email" type="text" />
-            <ContactField name="Country/Region" type="text" />
-            <ContactField name="Phone number (optional)" type="text" />
-            <ContactField name="Company (optional)" type="text" />
-            <ContactField name="Message" type="text" />
-            <button className="relative left-1/2 -translate-x-1/2 rounded-lg bg-blue-500 hover:bg-blue-400 text-white text-center mt-7 p-2 w-80 cursor-pointer transition-all">Submit</button>
+            <ContactField name="Email" type="text" placeholder="email@example.com" />
+            <ContactField name="Country/Region" type="text"/>
+            <ContactField name="Phone number (optional)" type="text"/>
+            <ContactField name="Company (optional)" type="text"/>
+            <p className="text-center text-xl font-light">Message</p>
+            <textarea
+                className="relative left-1/2 -translate-x-1/2 text-light w-80 min-h-50 px-2 py-1 mt-2 mb-5.5 rounded-lg border border-gray-300 dark:border-gray-700 resize-none overflow-y-auto">
+            </textarea>
+            <p/>
+
+            <button className="relative left-1/2 -translate-x-1/2 rounded-lg bg-blue-500 hover:bg-blue-400 text-white text-center mt-7 mb-10 p-2 w-80 cursor-pointer transition-all">Submit</button>
 
             <footer className="w-full text-center text-xl font-light mt-20 py-20 bg-gray-100 text-gray-600 dark:bg-gray-800 dark:text-gray-400">
                 <p className="mb-4">
